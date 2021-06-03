2a63230e0dbbd66b026929194387f25526b10646
Claudia C. Weber <claudia-c-weber@users.noreply.github.com>
2021-06-03 10:21:28 +0100

    Create category_labels_from_cont_manual.py

---
+++ utils/category_labels_from_cont_manual.py
@@ -0,0 +1,35 @@
+import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser(
+    description='Generate categorical integer labels by sequence ids from a continuous vector. Bin edges are specified manually')
+
+parser.add_argument(
+    "--idfiles", help="One or more files with sequence identifiers (one per line)")
+parser.add_argument(
+    "--feature", help="File with continuous vector to be binned (one per line)")
+parser.add_argument("--labelled", help="Destination for file with labels.")
+parser.add_argument("--bins", help="List of bin edges.")
+
+
+args = parser.parse_args()
+print(args)
+
+idfiles = args.idfiles.split()
+feature = args.feature.split()
+outfile = args.labelled
+bins = [int(i) for i in args.bins.split()]
+
+print(idfiles)
+
+ids = [open(f).read().split() for f in idfiles]
+ids = [y for x in ids for y in x]
+
+cont = [open(f).read().split() for f in feature]
+cont = np.array([float(y) for x in cont for y in x])
+
+
+bins = [0.] + bins + [np.max(cont)]
+digitized = np.digitize(cont, bins, right=True)
+
+np.savetxt(outfile, digitized, fmt='%i', delimiter="\n")
