fa6eb9a6940e9bf14f8ec149dbb747af9b9586ff
Claudia C. Weber <claudia-c-weber@users.noreply.github.com>
2023-02-08 15:01:13 +0000

    Add plotting util

---
+++ VAE/utils/vae_draw.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+import argparse
+
+##################################
+parser = argparse.ArgumentParser(
+    description='Produce 2D representation of a multidimensional numpy array')
+
+parser.add_argument(
+    "--zfile", help="2D array with reduced counts", required=True)
+parser.add_argument("--npy", help="Specify if input is a .npy file. Defaults to F (load from text)",
+                    default="F", choices=["T", "F"])
+parser.add_argument("--verbose", help="print extra model info",
+                    default="F", choices=["T", "F"])
+#TODO
+parser.add_argument("--labels", help="add file with labels", default="")
+parser.add_argument(
+    "--fignames", help="add file with figure names", default="figure")
+parser.add_argument("--superimpose", help="superimpose points",
+                    default="F", choices=["T", "F"])
+parser.add_argument("--categorical", help="Use contrasting categorical colours (defaults to gradient)",
+                    default="F", choices=["T", "F"])
+parser.add_argument("--outdir", help="Output directory", default="")
+parser.add_argument(
+    "--cmap", help="Custom colour map as RGB, e.g. \"#d60000 #8c3bff #018700\"", default="")
+parser.add_argument("--remove", help="Remove all classified points (requires --labels to be provided)",
+                    default="F", choices=["T", "F"])
+parser.add_argument("--canvas_size", help="Width/height of output image (square canvas)",
+                    type=int, default=1500, choices=[500, 1000, 1500])
+
+
+args = parser.parse_args()
+print(args)
+
+zfile = args.zfile
+categorical = args.categorical
+verbose = args.verbose
+labels = args.labels
+figures = args.fignames
+superimpose = args.superimpose
+outdir = args.outdir
+custom_cmap = args.cmap
+remove_classified = args.remove
+is_npy = args.npy
+
+canvas_size = args.canvas_size
+
+if custom_cmap != "":
+    print(custom_cmap)
+    custom_cmap = custom_cmap.split()
+    categorical = "T"
+    print(len(custom_cmap))
+
+########################################
+
+import pandas as pd
+import datashader as ds
+
+from holoviews.operation.datashader import datashade, dynspread
+import holoviews as hv
+
+import numpy as np
+
+hv.extension('bokeh')
+import colorcet as cc
+from matplotlib import cm, colors
+
+###########################################
+## cmap defaults
+default_cont_map = "viridis"
+default_cat_map = list(hv.Cycle.default_cycles["Colorblind"])
+longer_cat_map = cc.b_glasbey_hv
+
+##########################################
+
+
+def load_labels_filter(df, labels, remove_classified):
+    """Load labels, filter out all labels that do not equal zero if requested"""
+    df['label'] = pd.Series([int(i.strip()) for i in open(labels)])
+    if remove_classified == "T":
+        df = df[df['label'] == 0]
+    return df
+
+
+def load_coords(is_npy, zfile, labels, remove_classified):
+    """Load coordinates into dataframe from .npy or text"""
+    if is_npy == "F":
+        x_test_encoded = np.loadtxt(zfile, dtype="float32")
+    else:
+        x_test_encoded = np.load(zfile)
+
+    df = pd.DataFrame(data=x_test_encoded, columns=['x', 'y'])
+
+    if labels != "":
+        df = load_labels_filter(df, labels, remove_classified)
+
+    return df
+
+
+def set_colormap(n_labels):
+    """Set up colour maps, depending on whether a continuous or categorical map is needed.
+    If a custom map is provided, check that the number of colours matches the number of labels"""
+    if categorical == "F":
+        v = cm.get_cmap(default_cont_map)
+        return [colors.rgb2hex(i) for i in v(np.linspace(0, 1, n_labels))]
+    elif (custom_cmap != "") & (len(custom_cmap) == n_labels):
+        #TODO: Validity check?
+        return custom_cmap
+    elif n_labels <= len(default_cat_map):
+        return default_cat_map[:n_labels]
+    else:
+        print(
+            f"{n_labels} categories present, using longer colourmap. Consider merging or omitting some classes."
+        )
+        return longer_cat_map[:n_labels]
+
+def get_legend_labels(n_labels):
+    """Add text to legend labels (returns a dictionary)"""
+    return dict(zip(list(range(n_labels)), [f"bin {i}" for i in range(n_labels)]))
+
+
+#######################################################
+# Load data and render
+
+df = load_coords(is_npy, zfile, labels, remove_classified)
+
+# If no labels are provided, render using fire colormap
+if labels == "":
+    points = hv.Points(data=df, kdims=['x', 'y'])
+    hv.save(
+        dynspread(
+            datashade(points, cmap=cc.fire).opts(
+                bgcolor='black', width=canvas_size, height=canvas_size
+            )
+        ),
+        f'{outdir}{figures}.2d_plot.png',
+        fmt='png',
+    )
+else:
+    n_labels = np.max(df['label'])+1
+    colors_hex = set_colormap(n_labels)
+    points = hv.Points(data=df, kdims=['x', 'y'], vdims=['label'])
+
+    # If superimpose is set to False (default), render and shade all points together.
+    if superimpose == "F":
+        # Save plot
+        hv.save(
+            dynspread(
+                datashade(
+                    points,
+                    aggregator=ds.count_cat('label'),
+                    color_key=colors_hex,
+                ).opts(
+                    width=canvas_size,
+                    height=canvas_size,
+                    legend_labels=get_legend_labels(n_labels),
+                )
+            ),
+            f'{outdir}{figures}.2d_plot_labelled.png',
+            backend="bokeh",
+            fmt='png',
+        )
+    else:
+        points2 = hv.Points(data=df[df["label"] > 0], kdims=["x", "y"], vdims=["label"]).sort(
+            "label").options(color="label", cmap=dict(zip(list(range(1, n_labels)), colors_hex)))
+
+        hv.save(
+            dynspread(
+                datashade(points, cmap="grey").opts(
+                    bgcolor='white', width=canvas_size, height=canvas_size
+                )
+            )
+            * points2,
+            f'{outdir}{figures}.2d_plot_superimpose.png',
+            fmt='png',
+        )
